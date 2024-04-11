b40a9dddffcfbfe3268bc4927d4702ea13640746
saber-abd <fatima.72@hotmail.it>
2024-04-11 17:27:45 +0200

    Debut

---
+++ Test/Bownling/BowlingTest.java
@@ -0,0 +1,14 @@
+package Bownling;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BowlingTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
